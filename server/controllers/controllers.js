a8e50d7de21d6cf75751f0822cc88b6492b24b3d
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-05 21:35:21 +0400

    back- users and

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,16 +1,19 @@
-const  userSchema = require('../models/modelsSchema.js');
+const userSchema = require('../models/modelsSchema.js');
 const productSchema = require('../models/modelsSchema.js');
 const orderSchema = require('../models/modelsSchema.js');
 
 
 exports.createUser = async (req, res) => {
     try {
+        console.log(req.body);
+        
         const user = new userSchema(req.body);
         await user.save();
         res.status(201).json(user);
     } catch (error) {
         console.log("Failed to create user", error);
-    }}
+    }
+}
 
 exports.createProduct = async (req, res) => {
     try {
@@ -19,14 +22,45 @@ exports.createProduct = async (req, res) => {
         res.status(201).json(product);
     } catch (error) {
         console.log("Failed to create product", error);
-    }}
+    }
+}
 
-exports.createOrder = async (req, res) => { 
+exports.createOrder = async (req, res) => {
     try {
         const order = new orderSchema(req.body);
         await order.save();
         res.status(201).json(order);
     } catch (error) {
         console.log("Failed to create order", error);
-    }}  
+    }
+}
+
+
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await userSchema.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.log("Failed to fetch users", error);
+    }
+}
+
+
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await productSchema.find();
+        res.status(200).json(products);
+    } catch (error) {
+        console.log("Failed to fetch products", error);
+    }
+}
+
+exports.getOrders = async (req, res) => {
+    try {
+        const orders = await orderSchema.find();
+        res.status(200).json(orders);
+    } catch (error) {
+        console.log("Failed to fetch orders", error);
+    }
+}
 
--- server/router/routers.js
+++ server/router/routers.js
@@ -1,12 +1,15 @@
 
 const express = require('express');
-const { createUser, createProduct, createOrder} = require('../controllers/controllers.js');
+const { createUser, createProduct, createOrder, getOrders,getProducts,getUsers} = require('../controllers/controllers.js');
 
 const router = express.Router();
 
 router.post('/user', createUser);
 router.post('/product', createProduct);
 router.post('/order', createOrder);
+router.get('/users', getUsers);
+router.get('/products', getProducts);
+router.get('/orders', getOrders);
 
 
 module.exports = router;
