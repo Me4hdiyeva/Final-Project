b9103f8079a527eee0d0f241bcd92e756773f049
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-05 21:48:43 +0400

    add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Main from './layout/Main'
 
 function App() {
 
   return (
     <>
-   
+      <Routes>
+        <Route path='/' element={<Main/>}>
+          <Route />
+
+        </Route>
+
+      </Routes>
     </>
   )
 }
---
+++ client/src/layout/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { Outlet } from 'react-router'
+
+const Main = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet />
+      <Footer />
+
+    </>
+  )
+}
+
+export default Main
