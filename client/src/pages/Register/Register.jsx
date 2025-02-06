0ef7d83894ed2efd15c45551a9f3c9b4645c8ff4
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-06 22:39:28 +0400

    register form add

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+ import './Register.css'
 
 const Register = () => {
   return (
     <>
-      Register
+<div className='register'>
+    <input type="text" placeholder='Name' />
+    <input type="text"  placeholder='Username'/>
+    <input type="email"  placeholder='Email'/>
+    <input type="password"  placeholder='Password'/>
+    <input type="password"  placeholder='Confirm Password'/>
+    <input type="file" />
+    <button>Register</button>
+
+
+
+
+
+</div>
+
+
     </>
   )
 }
