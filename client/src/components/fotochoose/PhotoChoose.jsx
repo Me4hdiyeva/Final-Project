512fbba423a743bce6e749a239086772d94e58a7
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-10 04:39:29 +0400

    add about binance  section

---
+++ client/src/components/fotochoose/PhotoChoose.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import myImage from "../../img/img.png";
+import myImage2 from '../../img/img2.png';
+import myImage3 from '../../img/img3.png';
+import './PhotoChoose.css';
+
+const PhotoChoose = () => {
+    const [selectedImage, setSelectedImage] = useState('image1');
+
+    const images = {
+      image1: myImage,
+      image2: myImage2,
+      image3: myImage3,
+    };
+
+  return (
+    <>
+        <div style={{ textAlign: 'center' }}>
+            <div className='img-coin-a'>
+                <img className='img-coin-about' src={images[selectedImage]} alt="selected"  />
+            </div>
+            <div>
+                <button onClick={() => setSelectedImage('image1')}>Image 1</button>
+                <button onClick={() => setSelectedImage('image2')}>Image 2</button>
+                <button onClick={() => setSelectedImage('image3')}>Image 3</button>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default PhotoChoose;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,80 +2,130 @@ import React from 'react'
 
 import './Home.css'
 import NewListing from '../../components/newlisting/NewListing'
+import PhotoChoose from '../../components/fotochoose/PhotoChoose'
 // import QRScanner from '../../components/QRScanner.JSX'
+import Qrcode from '../../img/qrcode.png'
 
 const Home = () => {
   return (
     <>
       <div className="container">
+        <div className="sections">
+
+        <div className="first-sec">
         <div className="row">
           <div className="col-6">
-    <h1><span>333,333,333</span>
-    <br /> USERS <br />TRUST  US</h1>
-    <div className="email-phone-num-add-input">
-    <input className='phone-email-add' placeholder='Email/phone number' type="text" />
-    <button>Sign up</button>
-    </div>
+            <h1><span>333,333,333</span>
+              <br /> USERS <br />TRUST  US</h1>
+            <div className="email-phone-num-add-input">
+              <input className='phone-email-add' placeholder='Email/phone number' type="text" />
+              <button>Sign up</button>
+            </div>
 
-    <div className="footer-section">
-      <div className="row">
+            <div className="footer-section">
+              <div className="row">
 
-        <div className="col-6">
-<p>Or Continue  With</p>
-<div className="icons-google">
-<i class="fa-brands fa-google"></i>
-<i class="fa-brands fa-apple"></i>
-</div>
-        </div>
+                <div className="col-6">
+                  <p>Or Continue  With</p>
+                  <div className="icons-google">
+                    <i class="fa-brands fa-google"></i>
+                    <i class="fa-brands fa-apple"></i>
+                  </div>
+                </div>
 
-        <div className="col-6">
-<p>Download App</p>
-<div className="icons-google">
+                <div className="col-6">
+                  <p>Download App</p>
+                  <div className="icons-google">
 
-<i class="fa-solid fa-qrcode"></i>
-</div>
-{/* <QRScanner /> */}
+                    <i class="fa-solid fa-qrcode"></i>
+                  </div>
+                  {/* <QRScanner /> */}
 
-        </div>
-      </div>
-    </div>
+                </div>
+              </div>
+            </div>
 
-            
-</div>
+
+          </div>
           <div className="col-6">
             <div className="coins">
 
 
-<div className="coin">
+              <div className="coin">
 
-<NewListing/>
-
-
+                <NewListing />
 
 
 
 
 
-</div>
-<div className="coin">
-  <div className="hs">
- 
-  <h2>News</h2>
-  <h3>View All News</h3>
 
 
-  </div>
-  <p>Grayscale Announces Job Openings For Key Positions</p>
-  <p>Ethereum Name Service Sees Surge In January Registrations</p>
-  <p>CleanSpark Reports January Bitcoin Mining Output</p>
-  <p>Kentucky's Bitcoin Reserve Proposal Sparks Global <br /> Accumulation Speculation</p>
-</div>
+              </div>
+              <div className="coin">
+                <div className="hs">
+
+                  <h2>News</h2>
+                  <h3>View All News</h3>
+
+
+                </div>
+                <p>Grayscale Announces Job Openings For Key Positions</p>
+                <p>Ethereum Name Service Sees Surge In January Registrations</p>
+                <p>CleanSpark Reports January Bitcoin Mining Output</p>
+                <p>Kentucky's Bitcoin Reserve Proposal Sparks Global <br /> Accumulation Speculation</p>
+              </div>
 
             </div>
 
           </div>
 
 
+
+
+        </div>
+
+        </div>
+
+
+<div className="second-sec">
+          <div className="about-binance">
+        <div className="row">
+
+
+          <div className="col-6">
+            <PhotoChoose/>
+
+
+          </div>
+          <div className="col-6">
+            <h1>Trade on the go. Anywhere, anytime.</h1>
+            <div className="qr">
+
+            <img src={Qrcode} alt="code" />
+            <p>Scan to Download App <br /><h1 style={{
+              color:"white"
+            }}>iOS and Android</h1></p>
+
+            </div>
+            <div className="icons-about">
+            <i class="fa-brands fa-apple"></i>
+            <i class="fa-brands fa-windows"></i>
+            <i class="fa-brands fa-linux"></i>
+            </div>
+            <h4>More Download Options</h4>
+
+
+
+          </div>
+
+          </div>
+
+
+
+        </div>
+
+</div>
 
 
         </div>
