c0c71cd793586d58567bea7c5a30862350738f3e
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-13 23:08:40 +0400

    3rd section update

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -21,11 +21,11 @@ const List = () => {
       <div className="lists">
 
         <div>
-          <button onClick={() => handleClick(0)}>1.What is a cryptocurrency exchange?</button>
+          <button onClick={() => handleClick(0)}> <span>1</span> What is a cryptocurrency exchange?</button>
           {activeIndex === 0 && <p>Cryptocurrency exchanges are digital marketplaces that enable users to buy and sell cryptocurrencies like Bitcoin, Ethereum, and Tether. The Binance exchange is the largest crypto exchange by trade volume.</p>}
         </div>
         <div>
-          <button onClick={() => handleClick(1)}>2.What products does Binance provide?</button>
+          <button onClick={() => handleClick(1)}>  <span>2</span> What products does Binance provide?</button>
           {activeIndex === 1 && <p>Binance is the world's leading cryptocurrency exchange, catering to 235 million registered users in over 180 countries. With low fees and over 350 cryptocurrencies to trade, Binance is the preferred exchange to trade Bitcoin, Altcoins, and other virtual assets.
 
             With Binance users can:
@@ -36,16 +36,16 @@ const List = () => {
             Trade, stake, and loan NFTs on Binance NFT marketplace.</p>}
         </div>
         <div>
-          <button onClick={() => handleClick(2)}>3.How to buy Bitcoin and other cryptocurrencies on Binance</button>
+          <button onClick={() => handleClick(2)}> <span>3</span> How to buy Bitcoin and other cryptocurrencies on Binance</button>
           {activeIndex === 2 && <p>There are several ways to buy cryptocurrencies on Binance. You can use a credit/debit card, cash balance, or Apple Pay/Google Pay to purchase crypto on Binance. Before getting started, please make sure you’ve completed Identity Verification for your Binance account.</p>}
         </div>
         <div>
-          <button onClick={() => handleClick(3)}>4.How to track cryptocurrency prices</button>
+          <button onClick={() => handleClick(3)}> <span>4</span> How to track cryptocurrency prices</button>
           {activeIndex === 3 && <p>The easiest way to track the latest cryptocurrency prices, trading volumes, trending altcoins, and market cap is the Binance Cryptocurrency Directory. Click on the coins to know historical coin prices, 24-hour trading volume, and the price of cryptocurrencies like Bitcoin, Ethereum, BNB and others in real-time</p>}
         </div>
         <div>
           <button onClick={() => handleClick(4)}>
-           5. How to trade cryptocurrencies on Binance</button>
+            <span>5</span> How to trade cryptocurrencies on Binance</button>
           {activeIndex === 4 && <p>You can trade hundreds of cryptocurrencies on Binance via the Spot, Margin, Futures, and Options markets. To begin trading, users need to register an account, complete identity verification, buy/deposit crypto, and start trading.</p>}
         </div>
 
