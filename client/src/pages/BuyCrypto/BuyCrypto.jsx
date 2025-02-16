9df36284ed9a502c3d1622d8d41843f5b9de3922
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-17 03:44:06 +0400

    update  coins and add login

--- client/src/pages/BuyCrypto/BuyCrypto.jsx
+++ client/src/pages/BuyCrypto/BuyCrypto.jsx
@@ -7,32 +7,33 @@ import axios from 'axios'
 const BuyCrypto = () => {
 
 
-    const [ coins, setCoins ] = useState([])
+    const [coins, setCoins] = useState([])
 
-    const getHotCoins =async()=>{
-     const coins = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
-    //  console.log(coins.data);
-     
-     const top5Coins = coins.data.slice(0, 5);
-     console.log(top5Coins);
-     setCoins(top5Coins)
-     
+    const getHotCoins = async () => {
+        const coins = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+        //  console.log(coins.data);
+
+        const top5Coins = coins.data.slice(0, 5);
+        console.log(top5Coins);
+        setCoins(top5Coins)
+
 
     }
     useEffect(() => {
-        // İlk məlumat çəkmə
+        
         getHotCoins();
-    
-        // Hər 4 dəqiqədən bir məlumat çəkmək üçün interval təyin edirik (4 dəqiqə = 240,000 ms)
+
         const interval = setInterval(() => {
-          getHotCoins();
+            getHotCoins();
         }, 240000);
-    
-        // Cleanup - komponent silindikdə intervalı təmizləyirik
+
+       
         return () => clearInterval(interval);
-      }, []); // Yalnız bir dəfə mount olduğunda işləyir
+    }, []); 
 
 
+
+   
 
 
     return (
@@ -44,22 +45,23 @@ const BuyCrypto = () => {
                         <h3>Hot Cryptos</h3>
                         {
 
-                            coins.map((coin,index)=>(
+                            coins.map((coin, index) => (
 
-                        <div key={index} className="crypts">
-                            <div className="div-img">
-                                <img src={coin.image} alt="" />
-                               {coin.symbol}
-                            </div>
-                            <div className="price-crypyto">${coin.current_price}</div>
-                            <div className="procent-crypto">{coin.price_change_percentage_24h}</div>
-                        </div>
+                                <div key={index} className="crypts">
+                                    <div className="div-img">
+                                        <img src={coin.image} alt="" />
+                                        {coin.symbol}
+                                    </div>
+                                
+                                    <div className="price-crypyto">${coin.current_price}</div>
+                                    <div className="procent-crypto">{coin.price_change_percentage_24h}</div>
+                                </div>
                             ))
 
 
 
                         }
-                   
+
 
                     </div>
                 </div>
