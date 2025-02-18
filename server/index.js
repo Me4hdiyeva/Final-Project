1803b9f4bd3725a6a465e86ccfc08f36de715cc9
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-18 21:09:33 +0400

    gmail auth modify

--- server/index.js
+++ server/index.js
@@ -5,24 +5,22 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 const { default: mongoose } = require('mongoose');
-require("./googleAuthor"); // Passport konfiqurasiyasını burada daxil edirik
+require("./gmailAuth"); 
 
 const session = require("express-session");
 app.use(session({
-  secret: 'your-session-secret',  // secret olmalı və təhlükəsiz olmalıdır
+  secret: process.env.JWT_SECRET_KEY,  
   resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }  // HTTPS istifadə edirsinizsə, true edin
+  saveUninitialized: true
 }));
 
 const passport = require("passport");
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()  );
 
 const {userRouter} = require('./router/routers');
 
 app.use('/api/auth', userRouter);
-
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -30,6 +28,6 @@ mongoose.connect(MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Failed to connect to MongoDB:", error));
 
-app.listen(PORT, () => {
+app.listen(PORT , "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const { createUser, getUsers, loginUsers, deleteAllUsers } = require('../controllers/controllers.js');
 const { authenticateToken } = require('../middleware/verifyToken.js');
+const passport = require('passport');
 
 const router = express.Router();
+
+// router.get('/deluser', deleteAllUsers);
 
 router.get('/users', getUsers);
 router.post('/register', createUser);
 router.post('/login', loginUsers);
+
 router.get("/verify-token", authenticateToken, (req, res) => {
     try {
         res.status(200).send({ message: 'Token təstiqləndi!', status: true, user_id: req.user._id, username: req.user.username });
@@ -14,6 +18,14 @@ router.get("/verify-token", authenticateToken, (req, res) => {
         res.status(500).json(error)
     }
 })
-// router.get('/deluser', deleteAllUsers);
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        res.json({ message: "Logged in successfully!", user: req.user.user, token: req.user.token });
+    }
+);
+
+
 
 module.exports = router
