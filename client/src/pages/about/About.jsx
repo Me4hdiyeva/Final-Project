938ca3a33a8dc7a9e03b8fd03c40dbdad7959ad2
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-20 11:29:55 +0400

    css edit

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const About = () => {
     return (
         <>
-
             <div className="container">
 
                 <div className="about">
@@ -12,24 +11,15 @@ const About = () => {
                         <div className="col-6">
                             <h1>Welcome to Binance</h1>
                             <p>
-                            At Binance, we believe that everyone should have the freedom to earn, hold, spend, share and give their money - no matter who you are or where you come from.
+                                At Binance, we believe that everyone should have the freedom to earn, hold, spend, share and give their money - no matter who you are or where you come from.
                             </p>
                         </div>
                         <div className="col-6">
                             <img src="https://bin.bnbstatic.com/static/images/about/global-pc-2.png" alt="" />
                         </div>
-
-
                     </div>
-
-
-
                 </div>
-
-
-
             </div>
-
         </>
     )
 }
