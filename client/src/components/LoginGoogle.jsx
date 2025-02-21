b3dae9f4ff45fe895cc4046cc36c1b5d928eb8f0
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-22 02:37:08 +0400

    update dark-mode and css

--- client/src/components/LoginGoogle.jsx
+++ client/src/components/LoginGoogle.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../../src/index.css"
 
 const LoginGoogle = () => {
   const handleGoogleLogin = () => {
@@ -6,19 +7,20 @@ const LoginGoogle = () => {
   };
   return (
     <div>
-      <h2 className='text-center'>Google ilə daxil ol</h2>
-      <button style={{
+      {/* <h2 className='text-center'>Google ilə daxil ol</h2> */}
+      <button className='text-center'  style={{
         padding: "10px",
-        width: "90%",
-        border: "1px solid gray",
+        // width: "85%",
+        // border: "1px solid gray",
         maxWidth: "424px",
         borderRadius: "5px",
         display: "flex",
         gap: "120px",
-        paddingLeft: "15px",
+        paddingLeft: "25px",
+        marginLeft:"90px"
       }} onClick={handleGoogleLogin}><i style={{
         paddingTop: "5px"
-      }} className="fa-brands fa-google"></i>Google sdscsilə Daxil Ol</button>
+      }} className="fa-brands fa-google"></i>Google ilə daxil ol</button>
     </div>
   )
 }
