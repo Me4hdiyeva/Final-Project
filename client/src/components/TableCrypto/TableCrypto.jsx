2a1a6618b76412fe80fba333e5fabdc29b67d887
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-23 23:29:56 +0400

    update css

--- client/src/components/TableCrypto/TableCrypto.jsx
+++ client/src/components/TableCrypto/TableCrypto.jsx
@@ -21,7 +21,7 @@ const TableCrypto = () => {
 
     const getCoins = async () => {
         try {
-            const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd");
+            const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1");
             setCryptoData(response.data.slice(0,10));
         } catch (error) {
             console.error("Error fetching crypto data:", error);
