03b08a3dd00a56aaab93ce6b03399f25e62603b7
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-02-23 20:01:08 +0400

    deploy fix 10

--- client/src/components/TableCrypto/TableCrypto.jsx
+++ client/src/components/TableCrypto/TableCrypto.jsx
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import "../TableCrypto/tableCrypto.css"
+import "./tableCrypto.css"
 import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
